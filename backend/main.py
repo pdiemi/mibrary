7c7d4c0ae47744540b7b9d9c7603713cfdde8472
pdiemi <phamdiem0902@gmail.com>
2018-07-13 20:32:50 -0500

    imported backend to master, updated uml model

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+
+# --------------------------------------
+# app.py
+# --------------------------------------
+
+from flask import Flask
+from flask_cors import CORS
+import  pymysql
+from bridge import app
+from mibraryapi import api_page
+
+
+# app = Flask(__name__)
+app.register_blueprint(api_page)
+CORS(app)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True)
